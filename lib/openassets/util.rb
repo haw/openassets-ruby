87d2cd8ab2424a133915b2c5292c3593119bc43f
azuchi <azuchi@haw.co.jp>
2015-06-23 17:56:31 +0900

    add generate_asset_id method.

--- lib/openassets/util.rb
+++ lib/openassets/util.rb
@@ -2,18 +2,31 @@ module OpenAssets
   module Util
     include ::Bitcoin::Util
 
-    # namespace fo Open Asset
+    # namespace of Open Asset
     OA_NAMESPACE = 19
+
+    # version byte for Open Assets Address
+    OA_VERSION_BYTE = 23
 
     # convert bitcoin address to open assets address
     def address_to_oa_address(btc_address)
       btc_hex = decode_base58(btc_address)
       btc_hex = '0' +btc_hex if btc_hex.size==47
-      address = btc_hex[0..-9] # checksumを除いたBitcoin Address
+      address = btc_hex[0..-9] # bitcoin address without checksum
       named_addr = OA_NAMESPACE.to_s(16) + address
       oa_checksum = checksum(named_addr)
       encode_base58(named_addr + oa_checksum)
     end
 
+    # generate asset ID from public key.
+    def generate_asset_id(pub_key)
+      hash = hash160(pub_key)
+      # gen P2PKH script hash
+      # P2PKH script = OP_DUP OP_HASH160 <PubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+      # （76=OP_DUP, a9=OP_HASH160, 14=Bytes to push, 88=OP_EQUALVERIFY, ac=OP_CHECKSIG）
+      script = hash160(["76", "a9", "14", hash, "88", "ac"].join)
+      script = OA_VERSION_BYTE.to_s(16) + script # add version byte to script hash
+      encode_base58(script + checksum(script)) # add checksum & encode
+    end
   end
 end
--- spec/openassets/util_spec.rb
+++ spec/openassets/util_spec.rb
@@ -9,4 +9,9 @@ describe OpenAssets::Util do
     expect(util.address_to_oa_address("1MFW7BTwiNbAkmVz4SzAMQXboKYKGSzkq2")).to eq("akXDPMMHHBrUrd1fM756M1GSB8viVAwMyBk")
   end
 
+  it "generate asset ID from public key" do
+    expect(util.generate_asset_id(
+               "0450863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b23522cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"))
+      .to eq("ALn3aK1fSuG27N96UGYB1kUYUpGKRhBuBC")
+  end
 end
