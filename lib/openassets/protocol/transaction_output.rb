f15385a3c43e068d8c984189117aa17adfa53fed
azuchi <azuchi@haw.co.jp>
2015-07-16 17:18:19 +0900

    Add comment.

--- lib/openassets/protocol/transaction_output.rb
+++ lib/openassets/protocol/transaction_output.rb
@@ -9,6 +9,11 @@ module OpenAssets
       attr_accessor :asset_quantity
       attr_accessor :output_type
 
+      # @param [Integer] value The satoshi value of the output.
+      # @param [Bitcoin::Script] script The script controlling redemption of the output.
+      # @param [String] asset_id The asset ID of the output.
+      # @param [Integer] asset_quantity The asset quantity of the output.
+      # @param [OpenAssets::Transaction::OutPutType] output_type The type of the output.
       def initialize(value, script, asset_id, asset_quantity = 0, output_type = OutputType::UNCOLORED)
         raise ArgumentError, "invalid output_type : #{output_type}" unless OutputType.all.include?(output_type)
         raise ArgumentError, "invalid asset_quantity asset_quantity should be unsignd integer. " unless asset_quantity.between?(0, MarkerOutput::MAX_ASSET_QUANTITY)
