185b9858a25df634e891e4008d53ed92f1ccbf19
azuchi <azuchi@haw.co.jp>
2015-07-20 11:48:30 +0900

    add comment.

--- lib/openassets/transaction/transfer_parameters.rb
+++ lib/openassets/transaction/transfer_parameters.rb
@@ -4,15 +4,16 @@ module OpenAssets
     # The value object of a bitcoin or asset transfer.
     class TransferParameters
 
-      # Array of the unspent outputs available for the transaction.
       attr_accessor :unspent_outputs
-      # The asset quantity or amount of the satoshi sent in the transaction.
       attr_accessor :amount
-      # byte array of the output script to which to send any remaining change.
       attr_accessor :change_script
-      # byte array of the output script to which to send the assets or bitcoins.
       attr_accessor :to_script
 
+      # initialize
+      # @param [Array[OpenAssets::Transaction::SpendableOutput]] unspent_outputs Array of the unspent outputs available for the transaction.
+      # @param [String] to_script the output script to which to send the assets or bitcoins.
+      # @param [String] change_script the output script to which to send any remaining change.
+      # @param [Integer] amount The asset quantity or amount of the satoshi sent in the transaction.
       def initialize(unspent_outputs, to_script, change_script, amount)
         @unspent_outputs = unspent_outputs
         @to_script = to_script
