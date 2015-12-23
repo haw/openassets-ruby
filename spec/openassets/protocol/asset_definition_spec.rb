0354aee7c018cfcf0283e5378f53a939343a749d
azuchi <azuchi@haw.co.jp>
2015-12-23 17:28:44 +0900

    change test url.

--- spec/openassets/protocol/asset_definition_spec.rb
+++ spec/openassets/protocol/asset_definition_spec.rb
@@ -41,17 +41,17 @@ EOF
   end
 
   it 'parse url' do
-    definition = OpenAssets::Protocol::AssetDefinition.parse_url('https://cpr.sm/uNhUNQxM7-')
-    expect(definition.asset_ids.length).to eq(1)
-    expect(definition.asset_ids[0]).to eq('AboLrT5sHA1epmW2CL7UPqQ1AwwhomK8Si')
-    expect(definition.name_short).to eq('oat')
-    expect(definition.name).to eq('openassets-test')
-    expect(definition.contract_url).to eq('https://www.coinprism.info/asset/AboLrT5sHA1epmW2CL7UPqQ1AwwhomK8Si')
+    definition = OpenAssets::Protocol::AssetDefinition.parse_url('http://goo.gl/fS4mEj')
+    expect(definition.asset_ids.length).to eq(3)
+    expect(definition.asset_ids[0]).to eq('AGHhobo7pVQN5fZWqv3rhdc324ryT7qVTB')
+    expect(definition.name_short).to eq('HAWSCoin')
+    expect(definition.name).to eq('MHAWS Coin')
+    expect(definition.contract_url).to eq('http://techmedia-think.hatenablog.com/')
     expect(definition.issuer).to eq('Shigeyuki Azuchi')
-    expect(definition.description).to eq('for openassets-ruby test.')
+    expect(definition.description).to eq('カラーコインの実験用通貨です。')
     expect(definition.description_mime).to eq('text/x-markdown; charset=UTF-8')
     expect(definition.type).to eq('Currency')
-    expect(definition.divisibility).to eq(0)
+    expect(definition.divisibility).to eq(1)
     expect(definition.link_to_website).to be false
     expect(definition.icon_url).to be_nil
     expect(definition.image_url).to be_nil
