5938c9c93a33b588f050b74b2658e58ab85e2ee7
azuchi <azuchi@haw.co.jp>
2016-01-19 15:42:22 +0900

    Fix wrong argument.

--- spec/openassets/provider/bitcoin_core_provider_spec.rb
+++ spec/openassets/provider/bitcoin_core_provider_spec.rb
@@ -3,7 +3,7 @@ describe OpenAssets::Provider::BitcoinCoreProvider do
 
   it 'undefined api call' do
     provider = OpenAssets::Provider::BitcoinCoreProvider.new({})
-    expect(provider).to receive(:request).with('getinfo')
+    expect(provider).to receive(:request).with(:getinfo)
     provider.getinfo
   end
 
