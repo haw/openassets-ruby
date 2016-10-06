0bbaa97382ac3bec3714c9e6088908f14dbb286d
azuchi <azuchi@haw.co.jp>
2016-10-06 10:23:44 +0900

    Change issuance redeem script to verify check signature

--- lib/openassets/protocol/asset_definition_loader.rb
+++ lib/openassets/protocol/asset_definition_loader.rb
@@ -3,6 +3,7 @@ module OpenAssets
 
     class AssetDefinitionLoader
       extend Bitcoin::Util
+      extend OpenAssets::Util
 
       attr_reader :loader
 
@@ -20,17 +21,22 @@ module OpenAssets
 
       # create redeem script of asset definition file using p2sh
       # @param[String] url The asset definition url.
+      # @param[String] to The open asset address to send the asset to.
       # @return[Bitcoin::Script] redeem script.
-      def self.create_pointer_redeem_script(url)
+      def self.create_pointer_redeem_script(url, to)
         asset_def = "u=#{url}".bytes.map{|b|b.to_s(16)}.join
-        Bitcoin::Script.from_string("#{asset_def} OP_DROP")
+        btc_addr = oa_address_to_address(to)
+        script = Bitcoin::Script.from_string("#{asset_def}")
+        puts Bitcoin::Script.to_address_script(btc_addr).bth
+        Bitcoin::Script.new(script.append_opcode(Bitcoin::Script::OP_DROP).to_payload + Bitcoin::Script.to_address_script(btc_addr))
       end
 
       # create ps2s script which specify asset definition pointer
       # @param[String] url The asset definition url.
+      # @param[String] to The open asset address to send the asset to.
       # @return[Bitcoin::Script] p2sh script.
-      def self.create_pointer_p2sh(url)
-        redeem_script = create_pointer_redeem_script(url)
+      def self.create_pointer_p2sh(url, to)
+        redeem_script = create_pointer_redeem_script(url, to)
         Bitcoin::Script.new(Bitcoin::Script.to_p2sh_script(hash160(redeem_script.to_payload.bth)))
       end
     end
--- spec/openassets/protocol/asset_definition_loader_spec.rb
+++ spec/openassets/protocol/asset_definition_loader_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe OpenAssets::Protocol::AssetDefinitionLoader do
+describe OpenAssets::Protocol::AssetDefinitionLoader, :network => :testnet do
 
   describe 'initialize' do
 
@@ -26,16 +26,22 @@ describe OpenAssets::Protocol::AssetDefinitionLoader do
 
   describe 'create_pointer_redeem_script' do
     subject {
-      OpenAssets::Protocol::AssetDefinitionLoader.create_pointer_redeem_script('https://goo.gl/bmVEuw')
+      OpenAssets::Protocol::AssetDefinitionLoader.create_pointer_redeem_script('https://goo.gl/bmVEuw', 'bWwvzRQ6Lux9rWgeqTe91XwbxvFuxzK56cx')
     }
     it do
       expect(subject.chunks[0]).to eq('u=https://goo.gl/bmVEuw')
+      expect(subject.chunks[1]).to eq(Bitcoin::Script::OP_DROP)
+      expect(subject.chunks[2]).to eq(Bitcoin::Script::OP_DUP)
+      expect(subject.chunks[3]).to eq(Bitcoin::Script::OP_HASH160)
+      expect(subject.chunks[4]).to eq('46c2fbfbecc99a63148fa076de58cf29b0bcf0b0'.htb) # bWwvzRQ6Lux9rWgeqTe91XwbxvFuxzK56cx のBitcoinアドレスのhash160
+      expect(subject.chunks[5]).to eq(Bitcoin::Script::OP_EQUALVERIFY)
+      expect(subject.chunks[6]).to eq(Bitcoin::Script::OP_CHECKSIG)
     end
   end
 
   describe 'create_pointer_p2sh' do
     subject {
-      OpenAssets::Protocol::AssetDefinitionLoader.create_pointer_p2sh('https://goo.gl/bmVEuw')
+      OpenAssets::Protocol::AssetDefinitionLoader.create_pointer_p2sh('https://goo.gl/bmVEuw', 'bWwvzRQ6Lux9rWgeqTe91XwbxvFuxzK56cx')
     }
     it do
       expect(subject.is_p2sh?).to be true
