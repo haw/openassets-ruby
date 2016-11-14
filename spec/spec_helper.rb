7de6f9cde0fd7b89bc0547ed2a624365c164bc0d
azuchi <azuchi@haw.co.jp>
2016-11-14 10:50:29 +0900

    Refactoring the mock data loading tx

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,30 @@ RSpec.configure do |config|
     end
   end
 end
+
+def fixture_file(relative_path)
+  file = File.read(File.join(File.dirname(__FILE__), 'fixtures', relative_path))
+  JSON.parse(file)
+end
+
+def load_tx_mock(provider_mock)
+  Dir::entries(__dir__ + "/fixtures/tx").each do |file_name|
+    next unless file_name.include?(".json")
+    txid = file_name.delete(".json")
+    json = fixture_file("tx/#{txid}.json")
+    allow(provider_mock).to receive(:get_transaction).with(txid, 0).and_return(json['hex'])
+    allow(provider_mock).to receive(:get_transaction).with(txid, 1).and_return(json)
+    allow(provider_mock).to receive(:getrawtransaction).with(txid, 0).and_return(json['hex'])
+    allow(provider_mock).to receive(:getrawtransaction).with(txid, 1).and_return(json)
+  end
+end
+
+def load_block_mock(provider_mock)
+  Dir::entries(__dir__ + "/fixtures/block").each do |file_name|
+    next unless file_name.include?(".json")
+    block_hash = file_name.delete(".json")
+    json = fixture_file("block/#{block_hash}.json")
+    allow(provider_mock).to receive(:getblockhash).with(json['height']).and_return(block_hash)
+    allow(provider_mock).to receive(:getblock).with(block_hash).and_return(json)
+  end
+end
